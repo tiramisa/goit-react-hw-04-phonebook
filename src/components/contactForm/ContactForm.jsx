b440d8bf76cf337749b0e3d46fcd948dd6ea07c3
tiramisa <miller19988@gmail.com>
2023-08-29 00:22:34 +0100

    Update ContactForm.jsx

--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { nanoid } from 'nanoid';
 import styles from '../../myCss/index.module.css';
 
@@ -7,32 +7,30 @@ const INITIAL_STATE = {
   name: '',
 };
 
-export default class ContactForm extends Component {
-  state = INITIAL_STATE;
+const ContactForm = ({ onAdd, getUniqueContacts }) => {
+  const [formData, setFormData] = useState(INITIAL_STATE);
 
-  handleChangeForm = ({ target }) => {
+  const handleChangeForm = ({ target }) => {
     const { name, value } = target;
-    this.setState(prevState => ({
-      ...prevState,
+    setFormData(prevData => ({
+      ...prevData,
       [name]: value,
     }));
   };
 
-  handleFormSubmit = e => {
+  const handleFormSubmit = e => {
     e.preventDefault();
-    const { name, phone } = this.state;
-    const { onAdd } = this.props;
+    const { name, phone } = formData;
 
-    const isValidatedForm = this.validateForm();
+    const isValidatedForm = validateForm();
     if (isValidatedForm) {
       onAdd({ id: nanoid(), name, phone });
-      this.resetForm();
+      resetForm();
     }
   };
 
-  validateForm = () => {
-    const { name, phone } = this.state;
-    const { getUniqueContacts } = this.props;
+  const validateForm = () => {
+    const { name, phone } = formData;
     if (!name || !phone) {
       alert('Please fill in all fields!');
       return false;
@@ -40,38 +38,38 @@ export default class ContactForm extends Component {
     return getUniqueContacts(name);
   };
 
-  resetForm = () => this.setState(INITIAL_STATE);
+  const resetForm = () => setFormData(INITIAL_STATE);
 
-  render() {
-    const { name, phone } = this.state;
-    return (
-      <form className={styles.decorForm} onSubmit={this.handleFormSubmit}>
-        <input
-          className={styles.decorInput}
-          type="text"
-          name="name"
-          placeholder="Enter name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-          value={name}
-          onChange={this.handleChangeForm}
-        />
-        <input
-          className={styles.decorInput}
-          type="text"
-          name="phone"
-          placeholder="Enter phone number"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          required
-          value={phone}
-          onChange={this.handleChangeForm}
-        />
-        <button className={styles.decorButton} type="submit">
-          Add Contact
-        </button>
-      </form>
-    );
-  }
-}
+  const { name, phone } = formData;
+  return (
+    <form className={styles.decorForm} onSubmit={handleFormSubmit}>
+      <input
+        className={styles.decorInput}
+        type="text"
+        name="name"
+        placeholder="Enter name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+        value={name}
+        onChange={handleChangeForm}
+      />
+      <input
+        className={styles.decorInput}
+        type="text"
+        name="phone"
+        placeholder="Enter phone number"
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        required
+        value={phone}
+        onChange={handleChangeForm}
+      />
+      <button className={styles.decorButton} type="submit">
+        Add Contact
+      </button>
+    </form>
+  );
+};
+
+export default ContactForm;
